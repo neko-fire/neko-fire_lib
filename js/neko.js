07b98f95c2ab7d3b3323341a51b2fb92fd820bc3
Martin Bayer <martinbayer@Martins-MacBook-Pro.local>
2014-04-12 19:17:04 +0200

    refactoring, added new versions of libs, new js structure, added URL methods

--- js/neko.js
+++ js/neko.js
@@ -1,31 +1,71 @@
-/***
+/**
 * Neko-Fire Library
 *
 * requires: jQuery 1.6+
 * @author: Martin Bayer
 * neko-fire.com
-*
-*
-* planned features:
-* + cookie based theme engine for css
-* + customizable gallery in a lightbox
-***/
+*/
 
-// set up vars
+
+/**
+ * Top Level Namespace
+ * @type {NEKO|*|{}}
+ */
 var NEKO = window.NEKO || {};
 
 
-// get some user information
-NEKO.userConfig = {
-	// get browser dimensions
-	windowHeight: function() { return $(window).height(); },
-	windowWidth: function() { return $(window).width(); },
+/**
+ * GLOBAL function to extend Namespace
+ *
+ * @example extendNamespace(NEKO, 'module.submodule.subsubmodule')
+ *          extendNamespace(NEKO, 'module')
+ * @param namespace          Name of top level namespac, i.e. NEKO
+ * @param namespace_module   Name of module within namespace, i.e config
+ * @returns {*}
+ * @see http://addyosmani.com/blog/essential-js-namespacing/
+ */
+function extendNamespace( namespace, namespace_module ) {
+    'use strict';
+    var parts = namespace_module.split('.'),
+        parent = namespace,
+        partsLength,
+        i;
+    if (parts[0] === "myApp") {
+        parts = parts.slice(1);
+    }
+    partsLength = parts.length;
+    for (i = 0; i < partsLength; i++) {
+        //create a property if it doesn't exist
+        if (typeof parent[parts[i]] === 'undefined') {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];
+    }
+    return parent;
+}
 
-	docHeight: function() { return $(document).height(); },
-	docWidth: function() { return $(document).width(); }
-	
-};
 
+/**
+ * URL Utility Methods
+ */
+extendNamespace(NEKO,'utilities.URL');
+NEKO.utilities.URL = (function(){
+    'use strict';
 
-NEKO.nekoTheme = {};
-NEKO.nekoGallery = {};
+    /**
+     * get URL Parameter
+     * @param name name of URL Parameter to read
+     * @returns {string} decoded URL Parameter
+     */
+    function getURLParameter(name) {
+        return decodeURIComponent(
+            (new RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]);
+    }
+
+    // API
+    return {
+        getURLParameter : getURLParameter
+    };
+
+})();
+
